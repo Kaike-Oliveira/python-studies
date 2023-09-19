a5130640808f21d65e6bfa4b3d7d7c0f5c2c5ad9
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-18 21:02:06 -0300

    Introduction types

---
+++ udemy/types.py
@@ -0,0 +1,30 @@
+# This class was created to understand types on python
+
+# Python receive a dynamic and strong type style
+
+# String
+print('Hello world!')
+
+# Number
+
+# int => any number positive or negative and without signals
+print(13)
+print(-133)
+print(0)
+
+# float => any number with dot
+print(12.3, -0.9)
+
+# Boolean (bool)
+print(1 == 1)
+print(1 == 5)
+
+# type function can show the variable type
+
+print(type(12))
+print(type(1.2))
+print(type('12'))
+print(type(1 == 1))
+print(type(1 == 5))
+
+
