5f8925bc595decd578af7aa25346fcb8e69331c1
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-19 20:52:16 -0300

    exercises

--- udemy/exercises/variables.py
+++ udemy/exercises/variables.py
@@ -3,12 +3,13 @@
 name = 'Kaike'
 last_name = 'Oliveira'
 age = 18
-born_year = 2005
+born_year = 2023 - age
 is_adult = age >= 18
 height = 1.80
 
 print('Name:', name)
 print('Last name:', last_name)
 print('Age', age)
+print('Was born in:', born_year)
 print('Is adult?', is_adult)
 print('Height', height)
