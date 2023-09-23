c08e207f45fd07a32d8f326d799d2cd29579e0b1
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-23 18:04:54 -0300

    built-in

--- udemy/classes/built-in.py
+++ udemy/classes/built-in.py
@@ -2,9 +2,11 @@
 
 # Immutable types - str, int, float, bool
 
-string = 'Kaike'
+string = 'kaike'
 
 # (This can't happen because this is a immutable type!)
 # string[4] = 'change'
+# To do this we can do this!
 
-print(string[4])
+second_str = f'{string[:3]}change{string[4:]}'
+print(second_str.capitalize())
