ae102387984faa61cdcc0119f3da5822282a4163
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-23 17:58:09 -0300

    built-in

---
+++ udemy/classes/built-in.py
@@ -0,0 +1,10 @@
+# More python types
+
+# Immutable types - str, int, float, bool
+
+string = 'Kaike'
+
+# (This can't happen because this is a immutable type!)
+# string[4] = 'change'
+
+print(string[4])
