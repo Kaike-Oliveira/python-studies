7d6fa68ed368e3cfb367133ec3a439a34c55eeb2
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-23 21:13:58 -0300

    while break

---
+++ udemy/classes/while_break.py
@@ -0,0 +1,11 @@
+# While and Break
+# Execute an action when condition is true
+# Be careful this can cause a infinite looping
+
+condition = True
+
+while condition:
+    name = input('Type your name: ')
+    print(f'Your name is {name}!')
+
+    break
