4c9073a9141b3eff4da57685168aa0f5917b749b
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-25 19:38:00 -0300

    while break

--- udemy/classes/while_break.py
+++ udemy/classes/while_break.py
@@ -13,3 +13,16 @@ counter_2 = 10
 while counter_2 > 0:
     counter_2 -= 1
     print(counter_2)
+
+print(15 * '#')
+
+qtd_rows = 5
+qtd_cols = 5
+
+current_row = 1
+while current_row <= qtd_rows:
+    current_col = 1
+    while current_col <= qtd_cols:
+        print(f'{current_row=} {current_col=}')
+        current_col += 1
+    current_row += 1
