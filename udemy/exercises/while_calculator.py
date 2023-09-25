0dc8f965b4a82c7f8c556b0e629d3889f0316b0f
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-25 20:03:39 -0300

    while break

---
+++ udemy/exercises/while_calculator.py
@@ -0,0 +1,26 @@
+# Calculator with while
+
+start_ask = input('Do you like to start? [y] or [n] ')
+
+while True:
+    logout = input('Quit? [s] ').lower().startswith('s')
+    if logout:
+        break
+
+    num_1 = input('Type a number: ')
+    num_2 = input('Type other number: ')
+    operator = input('Type the operator: ')
+
+    valid_numbers = None
+
+    try:
+        num_1_float = float(num_1)
+        num_2_float = float(num_2)
+        valid_numbers = True
+    except Exception as error:
+        print(error)
+        valid_numbers = None
+
+    if valid_numbers is None:
+        print('Invalid number!')
+        continue
