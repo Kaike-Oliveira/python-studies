c9131af56b4f4af90085c35b710f5dfe922b6dc8
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-09-25 19:27:45 -0300

    while break

--- udemy/classes/while_break.py
+++ udemy/classes/while_break.py
@@ -5,5 +5,11 @@
 counter = 0
 
 while counter < 10:
-    counter = counter + 1
+    counter += 1
     print(counter)
+
+counter_2 = 10
+
+while counter_2 > 0:
+    counter_2 -= 1
+    print(counter_2)
