a6d697c74698e7c20fd38a63ae85a5a19d4e3ff9
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-05 21:41:35 -0300

    tuple

---
+++ udemy/classes/tuple.py
@@ -0,0 +1,9 @@
+# Tuple - immutable list
+
+names = ['Kaike', 'Helena', 'Clara', 'Carlos']
+tuple_ = tuple(names)
+list_ = list(tuple_)
+
+print('Default list in python: ', names, type(names))
+print('Converted tuple in python: ', tuple_, type(tuple_))
+print('Converted list in python: ', list_, type(list_))
