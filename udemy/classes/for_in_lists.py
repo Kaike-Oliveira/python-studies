0d528b87b1fc51dac81b1a6c1d61a5ba008e8155
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-05 21:27:26 -0300

    lists

---
+++ udemy/classes/for_in_lists.py
@@ -0,0 +1,11 @@
+# For in lists
+
+list_type = ['Kaike', 'João', 'Maria', 'Lara', 'Carlos', 'Pedro']
+
+list_type.sort()
+
+
+all_index = range(len(list_type))
+
+for index in all_index:
+    print(index, list_type[index])
