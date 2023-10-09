d525aa76d365e24312419cd7c4ffa27720545231
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-08 22:00:22 -0300

    CPF generator

--- udemy/exercises/cpf_generator.py
+++ udemy/exercises/cpf_generator.py
@@ -1,18 +1,41 @@
 # CPF generate
 
-cpf = '92316489001'
-nine_digit_1s = cpf[:9]
+cpf = input('Type the CPF here to consult: ')
+nine_digit = cpf[:9]
 
-# Calculate the first digit in CPF
+# Validate the first digit of CPF
 counter_1 = 10
 result_1 = 0
 
 first_digit = 0
 
-for digit_1 in nine_digit_1s:
-    first_digit += int(digit_1) * counter_1
+for digit in nine_digit:
+    first_digit += int(digit) * counter_1
 
     counter_1 -= 1
 
 digit_1 = (first_digit * 10) % 11
 digit_1 = digit_1 if digit_1 <= 9 else 0
+
+# Validate the second digit of CPF
+ten_digits = nine_digit + str(digit_1)
+
+counter_2 = 11
+second_digit = 0
+
+result_2 = 0
+
+for digit in ten_digits:
+    result_2 += int(digit) * counter_2
+
+    counter_2 -= 1
+
+digit_2 = (result_2 * 10) % 11
+digit_2 = digit_2 if digit_2 <= 9 else 0
+
+valid_cpf = f'{nine_digit}{digit_1}{digit_2}'
+
+if valid_cpf == cpf:
+    print('Valid CPF!')
+else:
+    print('Invalid CPF!')
