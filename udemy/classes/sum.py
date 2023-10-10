47b8ab2743f1bbde2f950e98256228b98a28ba2f
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-09 21:45:44 -0300

    Scope

--- udemy/classes/sum.py
+++ udemy/classes/sum.py
@@ -2,8 +2,13 @@
 
 # Can apply a default value to a arg
 def sum_def(x: float, y: float, z: float = 0):
-    print(x + y + z)
+    # Define a variable inside a function scope
+    sum_result = x + y + z
+
+    # Can return a result to a function
+    return sum_result
 
 
 # Named param
 sum_def(y=1, x=2)
+print(sum_def(y=1, x=2))
