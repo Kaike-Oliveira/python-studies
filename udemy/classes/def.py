dfde7215f32f510a1384da9c150dd63906969fb0
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-09 21:17:19 -0300

    Functions

---
+++ udemy/classes/def.py
@@ -0,0 +1,9 @@
+# Used to execute actions many times in code
+
+# The functions allow params in python, this is used in function call
+def show(param_1: str, param_2: str, param_3: str):
+    print(param_1, param_2, param_3)
+
+
+show('Testing', 'the param', 'of the funtion')
+print('*' * 20)
