cf5a6792b08ae0642eeba5a906a4a94ef4e13e7c
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-14 18:57:30 -0300

    filter in lists

--- udemy/classes/map-in-lists.py
+++ udemy/classes/map-in-lists.py
@@ -11,6 +11,7 @@ products = [
 products_update = [
     {**product, 'price': product['price'] + product['price'] * 10 / 100}
     for product in products
+    if product['price'] >= 1000  # filter the product when the price is 1000 or +
 ]
 
 print(*products_update, sep='\n')
