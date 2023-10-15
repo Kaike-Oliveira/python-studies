5242bcc8c8848d2418fa8c5474889d1ea7eca33c
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-15 12:01:23 -0300

    shopping list

--- udemy/exercises/shopping_list.py
+++ udemy/exercises/shopping_list.py
@@ -1,4 +1,5 @@
 # Ordered list
+import copy
 
 products = [
     {'name': 'Crusher', 'price': 12600},
@@ -8,5 +9,20 @@ products = [
     {'name': 'Washing', 'price': 16530},
 ]
 
-name_sorted_products = sorted(products.values)
-print(name_sorted_products)
+products_update = [
+    {**product, 'price': product['price'] + product['price'] * 10 / 100}
+    for product in products
+]
+
+price_sorted_products = sorted(copy.deepcopy(products_update), key=lambda product: product['price'])
+name_sorted_products = sorted(copy.deepcopy(products_update), key=lambda product: product['name'], reverse=True)
+
+
+def show_products(items):
+    for product in items:
+        print(product)
+
+
+show_products(name_sorted_products)
+print()
+show_products(price_sorted_products)
