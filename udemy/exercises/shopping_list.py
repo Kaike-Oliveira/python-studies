d66a3a5c6090cbb40813762dbb900dd8222a774c
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-10-15 10:11:17 -0300

    WIP

---
+++ udemy/exercises/shopping_list.py
@@ -0,0 +1,12 @@
+# Ordered list
+
+products = [
+    {'name': 'Crusher', 'price': 12600},
+    {'name': 'Screen', 'price': 8700},
+    {'name': 'Telestacker', 'price': 54300},
+    {'name': 'Roll', 'price': 970},
+    {'name': 'Washing', 'price': 16530},
+]
+
+name_sorted_products = sorted(products.values)
+print(name_sorted_products)
