72bb1c08efb745c982a3a574829ecf145acf906a
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-11-11 04:43:03 -0300

    model class

--- alura/OOP_2/model.py
+++ alura/OOP_2/model.py
@@ -21,11 +21,17 @@ class Model:
     def add_like(self):
         self._likes += 1
 
+    def show_details(self):
+        print(f'{show.name} | {show.year} | {show.likes}')
+
 
 class Movie(Model):
     def __init__(self, name: str, year: int, time: int):
         super().__init__(name, year)
         self.time = time
+
+    def show_details(self):
+        print(f'{show.name} | {show.year} | {self.time} | {show.likes}')
 
 
 class Serie(Model):
@@ -33,9 +39,14 @@ class Serie(Model):
         super().__init__(name, year)
         self.seasons = seasons
 
+    def show_details(self):
+        print(f'{show.name} | {show.year} | {self.seasons} | {show.likes}')
+
 
 avengers = Movie('avengers - infinite war', 2018, 160)
-print(f'{avengers.name} | {avengers.year} | {avengers.time} | {avengers.likes}')
-
 see = Serie('see', 2019, 2)
-print(f'{see.name} | {see.year} | {see.seasons} | {see.likes}')
+
+favorites = [avengers, see]
+
+for show in favorites:
+    show.show_details()
