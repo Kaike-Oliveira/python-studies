2b72c4f3d68feac90d1519f3e82f9a20858ba978
Kaike_Oliveira <kaikeoliveira196@gmail.com>
2023-11-12 22:32:23 -0300

    classes

--- udemy/classes/classes/log.py
+++ udemy/classes/classes/log.py
@@ -1,15 +1,43 @@
 # Log - Abstract
 
+# Imports
+from pathlib import Path
+
+# Define the file path
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
 class Log:
-    def log(self, message):
+    def _log(self, message):
         raise NotImplementedError('Add a log message!')
+
+    def log_error(self, message):
+        return self._log(f'Error: {message}')
+
+    def log_success(self, message):
+        return self._log(f'Success: {message}')
 
 
 class LogFileMixin(Log):
-    def log(self, message):
-        print(message)
+    def _log(self, message):
+        formatted_message = f'{message} ({self.__class__.__name__})'
+        print('Saving...')
+        with open(LOG_FILE, 'a') as file:
+            file.write(formatted_message)
+            file.write('\n')
+
+
+class LogPrintMixin(Log):
+    def _log(self, message):
+        print(f'Print: {message}')
 
 
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('Any thing')
+
+    log_print = LogPrintMixin()
+    log_print.log_error('Any thing')
+    log_print.log_success('Any thing')
+    
+    log_file = LogFileMixin()
+    log_file.log_error('Any thing')
+    log_file.log_success('Any thing')
--- udemy/classes/classes/main.py
+++ udemy/classes/classes/main.py
@@ -1,3 +1,14 @@
 # Main file
 
+# Imports
+from log import LogFileMixin, LogPrintMixin
+
 print('Main')
+
+log_print = LogPrintMixin()
+log_print.log_error('Any thing')
+log_print.log_success('Any thing')
+
+log_file = LogFileMixin()
+log_file.log_error('Any thing')
+log_file.log_success('Any thing')
